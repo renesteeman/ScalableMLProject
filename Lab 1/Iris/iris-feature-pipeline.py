eebae626acae98cc7ee511b436c0d366cde9b8b1
renesteeman <steeman.rene1@gmail.com>
2022-11-25 14:04:01 +0100

    lab2 notes

--- Lab 1/Iris/iris-feature-pipeline.py
+++ Lab 1/Iris/iris-feature-pipeline.py
@@ -10,7 +10,7 @@ def g():
     fs = project.get_feature_store()
     iris_df = pd.read_csv("https://repo.hops.works/master/hopsworks-tutorials/data/iris.csv")
     iris_fg = fs.get_or_create_feature_group(
-        name="titanic_modal",
+        name="iris_modal",
         version=1,
         primary_key=["sepal_length","sepal_width","petal_length","petal_width"], 
         description="Iris flower dataset")
